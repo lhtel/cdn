36ace2c7b947fd6cad0325bf2761a6cd471e8d1a
EricOffice <lhtel@163.com>
2021-11-20 15:27:52 +0800

    100090

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '1032366137246025'; 
-window.all_info['iappid'] = '1111611981';
+window.all_info['iadvplaceid'] = '6082261282373388'; 
+window.all_info['iappid'] = '1111596433';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php'
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/'
 );
 
 
