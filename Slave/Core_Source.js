ba56b334ac8c86142922ece7a3712c8d5877c2dc
EricOffice <lhtel@163.com>
2021-11-23 10:04:19 +0800

    100094

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '5072469293064350'; 
-window.all_info['iappid'] = '1111283873';
+window.all_info['iadvplaceid'] = '8092568218068906'; 
+window.all_info['iappid'] = '1111596433';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/'
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/'
 );
 
 
