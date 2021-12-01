0ef1d65761f8706309f84d0e0d077febb2d730c4
EricOffice <lhtel@163.com>
2021-12-01 09:44:45 +0800

    100107

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '9002562645312199'; 
-window.all_info['iappid'] = '1111596433';
+window.all_info['iadvplaceid'] = '4072966741849332'; 
+window.all_info['iappid'] = '1111689294';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/'
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/'
 );
 
 
