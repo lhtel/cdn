6fe2ec04f410bffa58a7a426ea898f0be998ebad
EricOffice <lhtel@163.com>
2021-12-12 10:08:04 +0800

    100124

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '1022971343919043'; 
-window.all_info['iappid'] = '1111596433';
+window.all_info['iadvplaceid'] = '2022176393566020'; 
+window.all_info['iappid'] = '1111689334';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/',
-'http://m.hivepe.com/'
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/'
 );
 
 
