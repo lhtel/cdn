036d406da051b22e21fa67ba444119d7ef7023f3
EricOffice <lhtel@163.com>
2021-12-13 16:24:56 +0800

    100127

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6082670314205679'; 
-window.all_info['iappid'] = '1111611999';
+window.all_info['iadvplaceid'] = '9062378326448106'; 
+window.all_info['iappid'] = '1111596423';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php'
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/',
+'http://m.ywdiyu.com/'
 );
 
 
