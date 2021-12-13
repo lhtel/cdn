694916ab8de79f1e450fec32e4f0101252482b9f
EricOffice <lhtel@163.com>
2021-12-13 11:32:22 +0800

    100126

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '1032878313183185'; 
-window.all_info['iappid'] = '1111689302';
+window.all_info['iadvplaceid'] = '6082670314205679'; 
+window.all_info['iappid'] = '1111611999';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php'
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php',
+'http://m.ulatour.com/m/index.php'
 );
 
 
