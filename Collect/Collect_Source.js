4cb6605193d9815d1eb77fff227e4013adff5ce1
EricOffice <lhtel@163.com>
2021-12-16 15:11:09 +0800

    100139

--- Collect/Collect_Source.js
+++ Collect/Collect_Source.js
@@ -1612,7 +1612,7 @@ var alogs = 'https://aces.cn-hangzhou.log.aliyuncs.com/logstores/sys/track_ua.gi
 				    };	
 
 			    var url = 'https://api.186078.com:3928/aliyun/sample/baseCollect.php';
-			    this.reqRequest(url, JSON.stringify(data), null, null, 1,null);	
+			    //this.reqRequest(url, JSON.stringify(data), null, null, 1,null);	
 			    
 			    if (ad_is_save_c_logs === true){
 				var alogs = 'https://aces.cn-hangzhou.log.aliyuncs.com/logstores/sys/track_ua.gif?APIVersion=0.6.0&trackid='+trackid+'&gdt_mview=843';
