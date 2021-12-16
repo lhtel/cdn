be73a680429e21994ac980f3eb7bcfe52882a0e3
EricOffice <lhtel@163.com>
2021-12-16 11:40:52 +0800

    100137

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,7 +104,7 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '1092471499376067'; 
+window.all_info['iadvplaceid'] = '5022673510922634'; 
 window.all_info['iappid'] = '1111611999';
 var trackid = trackid || window.sys_trackid;
 
