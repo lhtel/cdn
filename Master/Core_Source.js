e728b702d083ed5c8ede4b49ec6b570fe0a559d8
EricOffice <lhtel@163.com>
2021-12-21 22:23:19 +0800

    100153

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '7012478781867057'; 
-window.all_info['iappid'] = '1111611999';
+window.all_info['iadvplaceid'] = '6052577706328354'; 
+window.all_info['iappid'] = '1111689294';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php',
-'http://m.ulatour.com/m/index.php'
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/'
 );
 
 
