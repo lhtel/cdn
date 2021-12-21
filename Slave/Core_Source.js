f4be9cfd784e63a7776c710e1d6ec356bd816921
EricOffice <lhtel@163.com>
2021-12-22 06:44:03 +0800

    100154

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6052577706328354'; 
-window.all_info['iappid'] = '1111689294';
+window.all_info['iadvplaceid'] = '3002974766326766'; 
+window.all_info['iappid'] = '1111672762';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/'
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/'
 );
 
 
