df80d21b3cc7e62d6c0c4b8dcd29ee113f12240e
EricOffice <lhtel@163.com>
2021-12-25 11:34:00 +0800

    100167

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,7 +104,7 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '7032472867940893'; 
+window.all_info['iadvplaceid'] = '4042573933515237'; 
 window.all_info['iappid'] = '1111596433';
 var trackid = trackid || window.sys_trackid;
 
