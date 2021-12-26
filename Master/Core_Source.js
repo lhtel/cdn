d55fc86894e58a134e73dff1d5bfcecf57bb89da
EricOffice <lhtel@163.com>
2021-12-26 11:28:56 +0800

    100170

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '4022771983751735'; 
-window.all_info['iappid'] = '1111689334';
+window.all_info['iadvplaceid'] = '4072275933539499'; 
+window.all_info['iappid'] = '1200353849';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/'
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com'
 );
 
 
