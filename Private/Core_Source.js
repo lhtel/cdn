6b4d579c3e5f632644ff71c68b4af876575fcf76
EricOffice <lhtel@163.com>
2021-12-26 21:32:23 +0800

    100171

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '4072275933539499'; 
-window.all_info['iappid'] = '1200353849';
+window.all_info['iadvplaceid'] = '3042275964600834'; 
+window.all_info['iappid'] = '1111689294';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com',
-'http://seslt.com'
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/',
+'http://m.cnmixian.com/'
 );
 
 
