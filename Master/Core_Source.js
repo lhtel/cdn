c7838035e84253739a11197aa33428fc694587ca
EricOffice <lhtel@163.com>
2021-12-27 22:58:35 +0800

    100176

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '4002273957535132'; 
-window.all_info['iappid'] = '1111689294';
+window.all_info['iadvplaceid'] = '2032876908803676'; 
+window.all_info['iappid'] = '1200353849';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/',
-'http://m.cnmixian.com/'
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com',
+'http://seslt.com'
 );
 
 
