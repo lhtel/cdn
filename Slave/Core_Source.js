3c41cf33db80c34a9c1ed7cf5316a318add73218
EricOffice <lhtel@163.com>
2022-01-07 16:02:00 +0800

    100195

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6002362961814721'; 
-window.all_info['iappid'] = '1111596423';
+window.all_info['iadvplaceid'] = '9032869971515717'; 
+window.all_info['iappid'] = '1111689334';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/'
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/',
+'http://metootea.com/'
 );
 
 
