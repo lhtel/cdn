be64ec0540e28db59483fddb141e9abb3f5c3324
EricOffice <lhtel@163.com>
2022-01-18 11:44:55 +0800

    100212

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6022367941818733'; 
-window.all_info['iappid'] = '1111417629';
+window.all_info['iadvplaceid'] = '1062382747744647'; 
+window.all_info['iappid'] = '1200371644';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/',
-'http://m.xfzxs.com/'
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/'
 );
 
 
