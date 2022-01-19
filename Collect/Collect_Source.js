0d461827d3bd78a32e708e1f51f2d5c3703f9012
EricOffice <lhtel@163.com>
2022-01-19 17:47:55 +0800

    100213

--- Collect/Collect_Source.js
+++ Collect/Collect_Source.js
@@ -12,7 +12,7 @@ window.all_info['ad_list'] = [
 	{'iadvplaceid':"2052962911110768","iappid":"1111689326","time_out":"0","url":"http://m.favourai.com/"},
 	{'iadvplaceid':"3002382285223760","iappid":"1111596423","time_out":"0","url":"http://m.ywdiyu.com/"},
 	{'iadvplaceid':"8082583660056866","iappid":"1111596433","time_out":"0","url":"http://m.hivepe.com/"},
-	{'iadvplaceid':"4012864941216762","iappid":"1111672762","time_out":"0","url":"http://m.pandabibi.com/"},
+	{'iadvplaceid':"6092186815092711","iappid":"1111672762","time_out":"0","url":"http://m.pandabibi.com/"},
 	{'iadvplaceid':"5042764911417890","iappid":"1111689302","time_out":"0","url":"http://m.mkbake.com/m/index.php"},
 	{'iadvplaceid':"7002188565590021","iappid":"1111689294","time_out":"0","url":"http://m.cnmixian.com/"},
 	{'iadvplaceid':"8042688250939882","iappid":"1111611981","time_out":"0","url":"http://m.zhudive.com/m/index.php"},
