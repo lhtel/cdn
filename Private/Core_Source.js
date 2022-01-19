22e41a756d66b60eb3c21adb83dec0ac0da794e6
EricOffice <lhtel@163.com>
2022-01-19 17:52:47 +0800

    100214

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '4012864941216762'; 
-window.all_info['iappid'] = '1111672762';
+window.all_info['iadvplaceid'] = '7092986817699998'; 
+window.all_info['iappid'] = '1200371644';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/',
-'http://m.pandabibi.com/'
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/'
 );
 
 
