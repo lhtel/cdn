a8cdbdae46964aa71732a1468cc354e3ca73aa3a
EricOffice <lhtel@163.com>
2022-01-23 15:30:13 +0800

    100220

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '5042764911417890'; 
-window.all_info['iappid'] = '1111689302';
+window.all_info['iadvplaceid'] = '9012598062773720'; 
+window.all_info['iappid'] = '1200371644';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php',
-'http://m.mkbake.com/m/index.php'
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/'
 );
 
 
