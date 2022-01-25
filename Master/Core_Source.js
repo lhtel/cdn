be6c8b5b5df597eedc74aff2c1c540fa2892eaa0
EricOffice <lhtel@163.com>
2022-01-25 20:31:11 +0800

    100224

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '4082463991215785'; 
-window.all_info['iappid'] = '1111283873';
+window.all_info['iadvplaceid'] = '2042291100551358'; 
+window.all_info['iappid'] = '1200379683';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/',
-'http://3801wd.com/'
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/'
 );
 
 
