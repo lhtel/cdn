afb9533154a1549df1b5566506b71fe776e45a59
EricOffice <lhtel@163.com>
2022-01-30 11:53:18 +0800

    100230

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,8 +104,8 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '9042892257395935'; 
-window.all_info['iappid'] = '1200385360';
+window.all_info['iadvplaceid'] = '2022490268073681'; 
+window.all_info['iappid'] = '1200388470';
 var trackid = trackid || window.sys_trackid;
 
 
