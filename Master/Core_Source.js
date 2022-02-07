a42e656beff83a31e97560c491942dea39dbb128
EricOffice <lhtel@163.com>
2022-02-07 20:11:41 +0800

    100243

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,7 +104,7 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '7092596301586826'; 
+window.all_info['iadvplaceid'] = '6052098353280342'; 
 window.all_info['iappid'] = '1200379683';
 var trackid = trackid || window.sys_trackid;
 
