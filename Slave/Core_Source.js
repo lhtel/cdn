78001dfe0ad1f3a8328d7019a94a475ec6c4541c
EricOffice <lhtel@163.com>
2022-02-08 09:26:13 +0800

    100244

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6052098353280342'; 
-window.all_info['iappid'] = '1200379683';
+window.all_info['iadvplaceid'] = '8082583660056866'; 
+window.all_info['iappid'] = '1111596433';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/',
-'http://seslt.com/'
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/',
+'http://m.hivepe.com/'
 );
 
 
