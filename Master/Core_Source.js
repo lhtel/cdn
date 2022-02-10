0f69d9a6eb29d3736dc843fc52a72b862b5cacc7
QinJiaoJiao <HP@QinJiaoJiao.COM>
2022-02-10 08:06:56 +0800

    100247

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,7 +104,7 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '7042094316994530'; 
+window.all_info['iadvplaceid'] = '9022367950080459'; 
 window.all_info['iappid'] = '1111596433';
 var trackid = trackid || window.sys_trackid;
 
