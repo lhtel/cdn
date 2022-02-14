4a7b14017d0abba832a0720e1ed83353b948a890
EricOffice <lhtel@163.com>
2022-02-14 09:37:57 +0800

    100253

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '7012794054366160'; 
-window.all_info['iappid'] = '1111611981';
+window.all_info['iadvplaceid'] = '6092186815092711'; 
+window.all_info['iappid'] = '1111672762';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php',
-'http://m.zhudive.com/m/index.php'
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/',
+'http://m.pandabibi.com/'
 );
 
 
