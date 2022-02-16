9134d0b642d4132c1f644976b06e5fa5e9cab272
EricOffice <lhtel@163.com>
2022-02-16 13:55:34 +0800

    100257

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '3002382285223760'; 
-window.all_info['iappid'] = '1111596423';
+window.all_info['iadvplaceid'] = '6032296659149943'; 
+window.all_info['iappid'] = '1200353849';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/',
-'http://m.ywdiyu.com/'
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/',
+'http://seslt.com/'
 );
 
 
