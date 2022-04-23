19fdb1e12338c13a99459492712d06e099462eab
EricOffice <lhtel@163.com>
2022-04-23 09:55:19 +0800

    100291

--- Master/Core_Source.js
+++ Master/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '9023113709520498'; 
-window.all_info['iappid'] = '1110546630';
+window.all_info['iadvplaceid'] = '4033113729228416'; 
+window.all_info['iappid'] = '1110480309';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/',
-'http://6187wo.com/'
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/',
+'http://321dai.com/'
 );
 
 
