f64db4f73582f8cb87806e354ba29ada98b1aa8a
EricOffice <lhtel@163.com>
2022-04-29 10:49:59 +0800

    100296

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '9033616789028405'; 
-window.all_info['iappid'] = '1110558024';
+window.all_info['iadvplaceid'] = '1023423807685128'; 
+window.all_info['iappid'] = '1110344899';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/',
-'http://dashi88.com/'
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/',
+'http://xfzxs.com/'
 );
 
 
