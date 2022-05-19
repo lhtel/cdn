e8406eec34ec4ed992412e5ecb3efc06e799a76d
EricOffice <lhtel@163.com>
2022-05-19 12:20:55 +0800

    100310

--- Collect/Collect_Source.js
+++ Collect/Collect_Source.js
@@ -6,14 +6,8 @@ window.all_info = [];
 window.all_info['ad_list'] = [
 	{'iadvplaceid':"1063732964895061","iappid":"1111495254","time_out":"0","url":"http://6187wo.com/"},
 	{'iadvplaceid':"4033439934988966","iappid":"1200400636","time_out":"0","url":"http://metootea.com/"},
-	//{'iadvplaceid':"4033031860713472","iappid":"1111596433","time_out":"0","url":"http://hivepe.com/"},
-	//{'iadvplaceid':"1063539586128392","iappid":"1111689294","time_out":"0","url":"http://cnmixian.com/"},
-	//{'iadvplaceid':"1083533048272958","iappid":"1110480309","time_out":"0","url":"http://321dai.com/"},
+	{'iadvplaceid':"2053147096949018","iappid":"1111283873","time_out":"0","url":"http://3801wd.com/"},
 	{'iadvplaceid':"3093645026414902","iappid":"1200399809","time_out":"0","url":"http://favourai.com/"},
-	
-	//{'iadvplaceid':"7023220476214678","iappid":"1200500723","time_out":"0","url":"http://saibogame.com/"},
-	//{'iadvplaceid':"4043421406313649","iappid":"1200500722","time_out":"0","url":"http://guomiyh.com/"},
-	//{'iadvplaceid':"4093335250114287","iappid":"1200500722","time_out":"0","url":"http://guomiyh.com/"},
 ];
 
 var vl = new Array(
