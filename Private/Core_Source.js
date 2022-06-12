795c7d4fa5f93b9263b7239d844e393fee0a0d0c
EricOffice <lhtel@163.com>
2022-06-12 09:17:36 +0800

    100320

--- Private/Core_Source.js
+++ Private/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '6063550569599367'; 
-window.all_info['iappid'] = '1200591022';
+window.all_info['iadvplaceid'] = '7043455690991736'; 
+window.all_info['iappid'] = '1200577450';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/'
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/',
+'http://3801wd.com/'
 );
 
 
