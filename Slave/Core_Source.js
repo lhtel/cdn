6564b5016ecd9fcc562de025f97cf5830e1c8af9
EricOffice <lhtel@163.com>
2022-06-24 11:31:07 +0800

    100332

--- Slave/Core_Source.js
+++ Slave/Core_Source.js
@@ -104,27 +104,27 @@ window.curNode.parentNode.insertBefore(li_ifr10, window.curNode);
 window.s_ww = window.curNode.id ? parseInt(window.curNode.id) : 0;
 
 window.all_info = []; 
-window.all_info['iadvplaceid'] = '5093063120410822'; 
-window.all_info['iappid'] = '1200610214';
+window.all_info['iadvplaceid'] = '9053964184783251'; 
+window.all_info['iappid'] = '1200612829';
 var trackid = trackid || window.sys_trackid;
 
 
 var vl = new Array(
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/',
-'http://metootea.com/'
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/',
+'http://brophio.com/'
 );
 
 
